0fd74257b0b7c9da6be5f135d063f3f259e4a8a8
Alex Westphal <alexwestphal.nz@gmail.com>
2026-02-04 10:21:14 +1300

    Remove getStatParams from pub org layout.

--- src/app/pub/orgs/[slug]/layout.tsx
+++ src/app/pub/orgs/[slug]/layout.tsx
@@ -8,17 +8,9 @@
 import { AppSidebar } from "@/components/nav/app-sidebar";
 import { OrganizationProvider } from "@/hooks/use-organization";
 import { TITLE_SEPARATOR } from "@/lib/constants";
-import {
-    getAllOrganizationSlugs,
-    getOrganizationBySlug,
-} from "@/server/organization";
+import { getOrganizationBySlug } from "@/server/organization";
 
 import { Providers } from "../../../(authenticated)/providers";
-
-export async function generateStaticParams() {
-    const slugs = await getAllOrganizationSlugs();
-    return slugs.map((slug) => ({ slug }));
-}
 
 export async function generateMetadata(props: LayoutProps<`/pub/orgs/[slug]`>) {
     const { slug } = await props.params;
