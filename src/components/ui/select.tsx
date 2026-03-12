09ee2f9591dd57a01dcb4df5d5bb12b30068ece0
Alex Westphal <alexwestphal.nz@gmail.com>
2026-03-12 13:12:27 +1300

    Fixed broken select viewport.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -86,6 +86,7 @@ export function SelectContent({
             >
                 <SelectScrollUpButton />
                 <SelectPrimitive.Viewport
+                    data-position={position}
                     className={cn(
                         "data-[position=popper]:h-(--radix-select-trigger-height) data-[position=popper]:w-full data-[position=popper]:min-w-(--radix-select-trigger-width)",
                         position === "popper" && "",
